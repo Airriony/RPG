2da198401ed7d3ef595c741cba34e16ddd46f0bd
Airrion Young <ay@ay>
2018-03-19 16:53:53 -0700

    Version6

--- room5.c
+++ room5.c
@@ -4,9 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void dead(void);
+void level1(void);
+
 int main(void)
 {
-	int chest;
+	int chest = 0;
 	char lvlOne = 'a';
 
 	puts("Welcome to room #5");
@@ -16,23 +19,55 @@ int main(void)
 	puts("2: Blue Chest");
 	puts("3: Gold Chest");
 	
-	scanf("%d", &choice);
+	scanf("%d", &chest);
 
-	if(choice == 1)
+	if(chest == 1)
 	{
 		puts("A dragon has taken incinerated you");
 		puts("Game over");
 	}
-	if(choice == 2)
+	if(chest == 2)
 	{
 		puts("You have found the princess");
 		puts("The king has offered you a grand reward");
 
 		puts("Do you accept the kings reward? y-yes n-no");
-		scanf("%c", lvlOne);
+		scanf("%c", &lvlOne);
+
+		if(lvlOne == 'y')
+		{
+			level1();
+		}
+		if(lvlOne == 'n')
+		{
+			dead();
+		}
 	}
-	if(choice == 3)
+	if(chest == 3)
 	{
 		puts("You have waged war on a neighboring kingdom");
+	}
+
+	return 0;
+}
+void dead(void)
+{
+	puts("You have been killed");
+	puts("GAME OVER");
+}
+void level1(void)
+{
+	char marry = 'a';
+
+	puts("The king has rewarded you with the princesses hand in marriage");
+	puts("Do you accept? y-yes n-no");
+	if(marry == 'y')
+	{
+		puts("Your wedding is next thurday");
+		puts("CONGRATULATIONS");
+	}
+	if(marry == 'n')
+	{
+		dead();
 	}
 }
