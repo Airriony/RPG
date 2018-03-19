60de901094dc1143f3a1b86fbccc2c09ec13e9cf
Airrion Young <ay@ay>
2018-03-19 16:36:50 -0700

    Version2

--- room5.c
+++ room5.c
@@ -6,11 +6,29 @@
 
 int main(void)
 {
+	int choice;
+
 	puts("Welcome to room #5");
 
 	printf("Enter which chest to open");
 	puts("1: Red Chest");
 	puts("2: Blue Chest");
 	puts("3: Gold Chest");
+	
+	scanf("%d", choice);
 
+	if(choice == 1)
+	{
+		puts("A dragon has taken incinerated you");
+		puts("Game over");
+	}
+	if(choice == 2)
+	{
+		puts("You have found the princess");
+		puts("The king has offered you a grand reward");
+	}
+	if(choice == 3)
+	{
+		puts("You have waged war on a neighboring kingdom");
+	}
 }
